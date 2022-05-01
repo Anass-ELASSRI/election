da6717520bf4ca823db133d09ee8484c916f7f13
Anass-ELASSRI <101995821+Anass-ELASSRI@users.noreply.github.com>
2022-05-01 03:58:41 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,81 @@
+<?php
+$success = false;
+$message = false;
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    include 'dbh.php';
+
+    $email = $_POST["email"];
+    $motDePasse = $_POST["motDePasse"];
+    
+    $sql = "Select * from users where email='$email'";
+    $stmt = $pdo->query($sql);
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+    $isSuccess = 0;
+
+
+    if($user) {
+        $hashedPassword = $user["password_"];
+        if (password_verify($_POST["motDePasse"], $hashedPassword)) {
+            $isSuccess = 1;
+        }
+    }
+    if ($isSuccess == 0) {
+        $message = "Invalid Email or Password!";
+    } else {
+        header("Location:  ./logged.php");
+    }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inscription</title>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+<?php
+    if($message){
+        echo '<h2>'.$message.'</h2>';
+    }
+    if($success){
+        echo '<h2>inscri avec success</h2>';
+    }
+?>
+  <div class="form_wrapper">
+    <div class="form_container">
+      <div class="title_container">
+        <h2>Responsive Login Form</h2>
+      </div>
+      <div class="row clearfix">
+        <div class="">
+          <form method="post" action="" name="loginForm">
+            <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
+              <input type="email" name="email" placeholder="Email" required />
+            </div>
+            <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+              <input type="password" name="motDePasse" placeholder="Mot De Passe" required />
+            </div>
+
+            
+            <input class="button" type="submit" value="Login" />
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- <script src="https://use.fontawesome.com/4ecc3dbb0b.js"></script> -->
+  </body>
+
+
+</html>
