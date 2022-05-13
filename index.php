fb6c80339ddd21b0aab4e539b95758f62b989a9e
Anass-ELASSRI <101995821+Anass-ELASSRI@users.noreply.github.com>
2022-05-13 03:12:01 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -33,7 +33,95 @@ $voted = $stmt3->fetch(PDO::FETCH_OBJ);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Rank</title>
   <link rel="stylesheet" href="style.css">
+  <style>
+    .grid-container {
+      display: grid;
+      row-gap: 30px;
+      justify-content: space-between;
+      grid-template-columns: 30% 30% 30%;
+      padding: 10px;
+    }
 
+    .grid-item {
+      position: relative;
+      background-color: rgba(255, 255, 255, 0.8);
+      border: 1px solid #d2a834;
+      height: 350px;
+      display: grid;
+      grid-template-rows: 70% 30%;
+    }
+
+    .profil-pic {
+      background: red;
+      border-radius: 50%;
+      overflow: hidden;
+      border: 1px solid #f5ba1a;
+    }
+
+    .profil-pic img {
+      width: 100%;
+      height: auto;
+
+    }
+
+    .profil-text {
+      border-top: 1px solid #d2a834;
+      padding: 8px 7px;
+    }
+
+    .text-content {
+      font-size: 14px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: inline-block;
+      height: 65px;
+      text-align: center;
+    }
+
+    .overlay {
+      position: absolute;
+      bottom: 100%;
+      left: 0;
+      right: 0;
+      background-color: #f5ba1a;
+      overflow: hidden;
+      width: 100%;
+      height: 0;
+      transition: .5s ease;
+    }
+
+    .grid-item:hover .overlay {
+      bottom: 0;
+      height: 100%;
+    }
+
+    .overlay-content {
+      color: white;
+      font-size: 18px;
+      position: absolute;
+      overflow: hidden;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+      width: 100%;
+    text-align: center;
+    }
+
+    .btn-voter{
+      outline: none;
+    border: none;
+    background-color: #6c7676;
+    padding: 6px 25px;
+    font-size: 15px;
+    margin: auto;
+    color: #f0c445;
+    /* width: 100%; */
+    text-transform: uppercase;
+    margin-top: 10px;
+    cursor: pointer;
+    }
+  </style>
 </head>
 
 <body>
@@ -124,6 +212,7 @@ $voted = $stmt3->fetch(PDO::FETCH_OBJ);
     echo "<div><p class='voted_candidat'>vous avez deja vote à $candidat_voted->nom $candidat_voted->prenom</p></div>";
   }
   ?>
+  
 
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <script>
@@ -167,40 +256,39 @@ $voted = $stmt3->fetch(PDO::FETCH_OBJ);
     }
   </script>
 
-<style>
-.grid-container {
-  display: grid;
-  gap: 20px 100px;
-  grid-template-columns: 1fr 1fr 1fr;
-  background-color: #2196F3;
-  padding: 10px;
-}
-.grid-item {
-  background-color: rgba(255, 255, 255, 0.8);
-  border: 1px solid rgba(0, 0, 0, 0.8);
-  height: 230px;
-  display: grid;
-  grid-template-rows: 2fr 1fr;
-}
-.profil-pic {
-  background-color: red;
-}
-</style>
-<div class="container">
+  
+<?php
+  if (!$voted) {
+    echo '<div class="container">
+             <div class="grid-container">';
+      foreach ($candidats as $candidat) {
+        echo "  <div class='grid-item'>
+                <div class='profil-pic'>";
+                if($candidat->sexe == 'H'){
+                  echo "<img src='./img/man.png'>";
+                }else{
+  echo "<img src='./img/woman.png'>";
 
-<div class="grid-container">
-  <?php
-    foreach ($candidats as $candidat) {
-      echo "  <div class='grid-item'>
-                <div class='profil-pic'>
-                  
+}
+               echo "</div>
+                <div class='profil-text'>
+                  <div class='text-content'>
+                    $candidat->text_presentation
+                  </div>
                 </div>
-                <div class='profil-text'></div>
+                <div class='overlay'>
+                <div class='overlay-content'>
+                
+                <div class='name'>$candidat->prenom $candidat->nom</div>
+                <button onclick='voter($candidat->id)' class='btn-voter'>Voter</button>
+                </div>
+                </div>
               </div>";
+      }
+    echo '</div>
+  </div>';
     }
   ?>
-</div>
-</div>
 </body>
 
 </html>
